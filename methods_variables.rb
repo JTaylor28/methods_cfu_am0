6262642e7f4aa78e1df0588602618fad6468fbc5
James Taylor <jrtaylor0614@gmail.com>
2022-11-03 14:05:09 -0600

    Comments out question 1 and begins qestion 2)

--- methods_variables.rb
+++ methods_variables.rb
@@ -3,15 +3,25 @@
 
 # Define a variable that stores a string
 
+   # A variable that stores a string is an object that is linked to a data type. 
+   # Specifically a data type that contains word or fraises
+
 #  call upcase on the variable, print it out
 #  call downcase on the variable, print it out
 #  call reverse on the variable, print it out
 #  call length on the variable, print it out
 
+# variable = "got it!"
+# p variable.upcase
 
+# variable2 = "got it!"
+# p variable2.downcase
 
+# variable3 = "got it!"
+# p variable3.reverse 
 
-
+# variable4 = "got it!"
+# p variable4.length 
 
 
 # =================================
@@ -22,6 +32,14 @@ last_login = "12/09/2021"
 
 # 1: In the Ruby Docs for String methods (https://ruby-doc.org/core-3.1.0/String.html), find 4 methods you have not yet used and call them on one of the variables above. Between reading the documentation and reading the output from calling the methods, make sense of what they do. 
   # Write the methods out on the lines below, with an explanation in your own words of how they work.
+
+new_user_name = user_name.sub(/coco/,"james")
+p user_name
+p new_user_name 
+p user_name
+
+user_name.sub!(/coco/,"james")
+p user_name
 
 
 
