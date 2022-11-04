f1b3e88f8046ed7adc6f8636a39e0c3f6610d2af
James Taylor <jrtaylor0614@gmail.com>
2022-11-03 19:00:26 -0600

    attempts to completefinal

--- final_practice.rb
+++ final_practice.rb
@@ -1,36 +1,72 @@
-# Defining Our Own Methods
-# 1: Write a method named greeting that prints out a general greeting to someone
+# # Defining Our Own Methods
+# # 1: Write a method named greeting that prints out a general greeting to someone
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-
-
-
-
-#2: Write a method named custom_greeting that prints out a greeting WITH a specific name.
+# def greeting
+#     return "hello, someone"
+# end
+# p greeting
 
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# # What is the return value of your method?
+
+# # "hello, someone"
+
+# # How many arguments did you pass your method?
+
+# # not a one 
+
+
+# #2: Write a method named custom_greeting that prints out a greeting WITH a specific name.
+
+# def custom_greeting(name)
+#     "Whats up, #{name}"
+# end
+# p custom_greeting("james")
+
+# # What is the return value of your method?
+
+# # "whats up, James"
+
+# # How many arguments did you pass your method?
+
+# # one
+
+# # What data type was your argument(s)?
+
+# # string
+
+# #3: Write a method named square that takes in one number, and returns the square of that number
+
+# def square(num)
+#     num ** 2
+# end
+# print square(4)
+
+# # What is the return value of your method?
+
+# # 16
+
+# # How many arguments did you pass your method?
+
+# # one
+
+# # What data type was your argument(s)?
+
+# # intager 
+
+# #4: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and print outs the sentence of the entire string
 
 
 
+def greet_person(first_name, middle_name, last_name)
+   p "whats up, #{first_name + " " + middle_name + " " + last_name}!"
+end
 
-#3: Write a method named square that takes in one number, and returns the square of that number
+# first_name = "Billy"
+# middle_name = "Bob"
+# last_names = "joe"
 
-
-
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
-
-
-
-
-#4: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and print outs the sentence of the entire string
-
-
+greet_person("Billy","Bob" "Joe")
 
 # What is the return value of your method?
 # How many arguments did you pass your method?
